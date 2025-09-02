fde26e1c32f8308220e7075c3bc82e8614def469
markrod1029 <103241234+markrod1029@users.noreply.github.com>
2025-09-03 00:30:46 +0800

    tailwind

---
+++ frontend/postcss.config.js
@@ -0,0 +1,13 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+// Kung hindi naka-ESM (type":"module sa package.json)
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
